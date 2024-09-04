2353bf577710eb69ca6d451f3b72d33788b5d976
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-04 09:53:42 -0300

    Area

---
+++ src/introducao/exercicios/Ex2.java
@@ -0,0 +1,21 @@
+package introducao.exercicios;
+
+import  java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+        System.out.println("SISTEMA DE CALCULAR O RAIO DO CIRCULO");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o valor do Raio");
+        double raio = scan.nextDouble();
+
+        double PI = 3.14159;
+        double area = PI * (raio * raio);
+
+        System.out.println("O valor da área do circulo é: " + area);
+
+
+    }
+}
