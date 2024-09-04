aa3dc69b5c91daa1de1129bfd700bae87930de42
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-04 19:09:40 -0300

    exercicios

---
+++ src/introducao/exercicios/Ex9.java
@@ -0,0 +1,18 @@
+package introducao.exercicios;
+
+import  java.util.Scanner;
+public class Ex9 {
+    public static void main(String[] args) {
+
+        // contagem regressiva com for
+        for (int i = 3; i >=0  ; i--){
+            System.out.println(i);
+        }
+
+        // contagem crescente com for
+        for(int i = 0; i <=3 ; i++){
+            System.out.println(i);
+        }
+
+    }
+}
