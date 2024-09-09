734e60b4b5bb7d0c5008c3dac54f6e8c1c2b84a6
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-09 14:53:49 -0300

    encapsulamento

--- src/poo/membrosEstaticos/application/Program.java
+++ src/poo/membrosEstaticos/application/Program.java
@@ -1,21 +1,21 @@
 package poo.membrosEstaticos.application;
 import poo.membrosEstaticos.util.Calculator;
 import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Calculator calc = new Calculator();
 
         System.out.print("Digite o Raio: ");
 
         double radius = scan.nextDouble();
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Circumference: %.2f%n", v);
-        System.out.printf("PI: %.2f%n", calc.PI);
+        System.out.printf("PI: %.2f%n", Calculator.PI);
 
     }
 
--- src/poo/membrosEstaticos/util/Calculator.java
+++ src/poo/membrosEstaticos/util/Calculator.java
@@ -1,13 +1,16 @@
 package poo.membrosEstaticos.util;
 
 public class Calculator {
-    public final double PI = 3.14159;
 
-    public  double circumference(double radius){
+
+    // valores estaticos que não mudam ou não precisam ser modificados para cada caso de calculo etc...
+    public static final double PI = 3.14159;
+
+    public static double circumference(double radius){
         return  2.0 * PI * radius;
 
     }
-    public  double volume(double radius){
+    public static double volume(double radius){
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 
