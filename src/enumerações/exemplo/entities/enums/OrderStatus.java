11bf33c67b073d07ca793bea541a8bd89216060d
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-25 21:24:52 -0300

    Update OrderStatus.java

--- src/enumerações/exemplo/entities/enums/OrderStatus.java
+++ src/enumerações/exemplo/entities/enums/OrderStatus.java
@@ -1,12 +1,13 @@
 package enumerações.exemplo.entities.enums;
 
+// Declaração de uma enumeração chamada 'OrderStatus', que contém os diferentes estados possíveis de um pedido.
 public enum OrderStatus {
 
+    // Definição dos valores da enumeração. Cada um desses valores representa um estado possível de um pedido.
     PAGAMENTO_PENDENTE,
     PROCESSANDO,
     ENVIADO,
     ENTREGUE,
-    ;
 
-
+    ; // O ponto e vírgula aqui é opcional neste caso, mas pode ser necessário se houver métodos ou outros membros na enum.
 }
