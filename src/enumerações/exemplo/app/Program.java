309f9e0f9a6936ff7d20fff52b618416581edee6
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-09-25 21:24:02 -0300

    Update Program.java

--- src/enumerações/exemplo/app/Program.java
+++ src/enumerações/exemplo/app/Program.java
@@ -7,16 +7,18 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Pedido pedido = new Pedido(1080,new Date(),OrderStatus.PAGAMENTO_PENDENTE);
+        // Cria um novo objeto 'Pedido'
+        Pedido pedido = new Pedido(1080, new Date(), OrderStatus.PAGAMENTO_PENDENTE);
         System.out.println(pedido);
 
+        // Atribui à variável 'os1' o valor 'ENTREGUE' da enumeração 'OrderStatus'.
         OrderStatus os1 = OrderStatus.ENTREGUE;
+
+        // Usa o método 'valueOf' da classe 'Enum' para converter a string "ENTREGUE" em um valor da enumeração 'OrderStatus'.
+        // O valor retornado é equivalente a 'OrderStatus.ENTREGUE'.
         OrderStatus os2 = OrderStatus.valueOf("ENTREGUE");
 
         System.out.println(os1);
         System.out.println(os2);
-
-
-
     }
 }
