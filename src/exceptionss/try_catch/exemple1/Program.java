27b0f3120f1aa2fd148ffdbd0d2d7b0581ecab20
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-10-07 19:52:48 -0300

    Update Program.java

--- src/exceptionss/try_catch/exemple1/Program.java
+++ src/exceptionss/try_catch/exemple1/Program.java
@@ -7,6 +7,23 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+        method1();
+
+        System.out.println("End program");
+
+    }
+    //TRY CATCHS
+        public static void method1(){
+
+            System.out.println("*** METODO 1 start ***");
+            method2();
+            System.out.println("*** METODO 1 END ***");
+
+
+
+        }
+    public static void method2(){
+        System.out.println("*** METODO 2 start ***");
         Scanner scan = new Scanner(System.in);
 
         // bloco que pode gerar erro
@@ -16,21 +33,19 @@ public class Program {
             System.out.println(vect[position]); // erro, colocando uma posição inexistente
         }
 
+        // bloco para caso ocrroa o erro esperado
         catch (ArrayIndexOutOfBoundsException erro) { // nome do erro
             System.out.println("Invalid Position:(ArrayIndexOutOfBoundsException)");
+
+            // mostra a sequencia de chamadas que deram erro
+            erro.printStackTrace();
+
+            scan.next();
         }
         catch (InputMismatchException erro) {
             System.out.println("Input Error :(InputMismatchException)");
         }
-
-        System.out.println("End program");
-
-
-
-
-
-
-        scan.close();
+        System.out.println("*** METODO 2 END ***");
 
     }
 }
